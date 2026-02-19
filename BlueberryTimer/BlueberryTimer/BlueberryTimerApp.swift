581ebeccceb7481921450569bba6f01f6bb08788
Dhara Santos <santtosdhara@gmail.com>
2026-02-19 15:13:34 -0500

    Initial project setup

---
+++ BlueberryTimer/BlueberryTimer/BlueberryTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlueberryTimerApp.swift
+//  BlueberryTimer
+//
+//  Created by Dhara Chavez on 2/19/26.
+//
+
+import SwiftUI
+
+@main
+struct BlueberryTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
